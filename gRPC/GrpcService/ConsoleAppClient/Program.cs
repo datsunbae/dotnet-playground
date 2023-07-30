533604a696ec8b0f6289e0f1265560c78f355f0f
kodoku-x <datsunbae@gmail.com>
2023-07-30 09:52:55 +0700

    :hammer: gRPC

---
+++ gRPC/GrpcService/ConsoleAppClient/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcService;
+
+var message = new HelloRequest
+{ 
+    Name = "Datsunbae"
+};
+var channel = GrpcChannel.ForAddress("http://localhost:5267");
+var client = new Greeter.GreeterClient(channel);
+var reponse = await client.SayHelloAsync(message);
+Console.WriteLine(reponse.Message);
+Console.ReadLine();
